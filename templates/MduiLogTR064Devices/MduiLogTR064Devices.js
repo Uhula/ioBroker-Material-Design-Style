41aef5281faa4dadb9a92b2849ca2b27d7fab956
Uwe Uhula <uwe@uhula.de>
2020-05-01 11:44:05 +0200

    Update MduiLogTR064Devices
    2020.05.01 UH
    * Anpassung an neues MduiBase (intern)
    * Anpassung an MDCSS 2.5
    * Einbau Sortieren der Table über Header-Click

--- templates/MduiLogTR064Devices/MduiLogTR064Devices.js
+++ templates/MduiLogTR064Devices/MduiLogTR064Devices.js
@@ -34,6 +34,14 @@ sortAscending  : true -> aufsteigend sortieren, sonst absteigend
 
 **** Lizenz
 (c) 2020 by UH, MIT License, no warranty, use on your own risc
+
+*** Changelog
+2020.05.01 UH 
+* Anpassung an neues MduiBase (intern)
+* Anpassung an MDCSS 2.5
+* Einbau Sortieren der Table über Header-Click
+
+
 */
 
 
@@ -45,13 +53,6 @@ class MduiBase {
 
     constructor() {
       this.init();
-      // beim 1.Start nur die States erzeugen
-      if ( !this.existState("version") || (this.getState('version').val!=this.VERSION) ) {
-          for (let s=0; s<this.states.length; s++) { this.createState( this.states[s].id ); }
-          this.logWarn('first script start, create states for version '+this.VERSION+', please wait 5 seconds and start script again');
-          setTimeout( setState, 3000, this.STATE_PATH + 'version', this.VERSION );
-      }
-      else this.installed = true; 
     }
     
     //
@@ -61,10 +62,19 @@ class MduiBase {
         this.VERSION    = '1.0/2020-01-01';
         this.NAME       = 'mduiBase';
         this.STATE_PATH = '0_userdata.0.mdui.base.';
+        this.STATE_UNKNOWN    =  0;
+        this.STATE_INSTALLING = 10;
+        this.STATE_INSTALLED  = 11;
+        this.STATE_STARTING   = 20;
+        this.STATE_STARTED    = 21;
+        this.STATE_STOPPING   = 30;
+        this.STATE_STOPPED    = 31;
     
         // var
-        this.installed = false;
+        this.state = this.STATE_UNKNOWN;
         this.states = [];
+        this.subscribers = [];
+        this.schedulers = [];
     
         this.doInit();
     
@@ -72,30 +82,87 @@ class MduiBase {
         this.states.push( { id:'version',     common:{name:'installed script-version', write:false, def:this.VERSION} } );
     }
     
+    //
     // start the script/class
+    //
     start() {
-        if (!this.installed) {
-            this.logWarn('cant start, states for version '+this.VERSION+' missed, please start script again');
+        // beim 1.Start nur die States erzeugen
+        if ( !this.existState("version") || (this.getState('version').val!=this.VERSION) ) {
+            for (let s=0; s<this.states.length; s++) { this.createState( this.states[s].id ); }
+            this.logWarn('first script start, creating states for version '+this.VERSION+', automatic restarting script again in 10 sec ...');
+            setStateDelayed(this.STATE_PATH + 'version', this.VERSION, 3000);
+            setTimeout( this.start.bind(this), 10000 );
+            this.state = this.STATE_INSTALLED; 
             return;
+        }
+        switch (this.state) {
+            case this.STATE_UNKNOWN : ;
+            case this.STATE_INSTALLING : ;
+            case this.STATE_INSTALLED : ;
+            case this.STATE_STOPPED : {
+                this.state = this.STATE_STARTING; 
+                if (this.doStart()) {
+                    this.log('script started');
+                    this.state = this.STATE_STARTED;
+                }
+                break;    
+            }
+            case this.STATE_STARTING : ;
+            case this.STATE_STARTED : {
+                this.logWarn('script already starting/started');
+                break;    
+            }
+            case this.STATE_STOPPING : {
+                this.logWarn('script is stopping, pls start later again');
+                break;    
+            }
+      
         } 
-        if (this.doStart())
-            this.log('script started');
     }
     
+    //
     // stop the script/class
+    //
     stop() {
-        if (this.doStop())
-            this.log('script stopped');
+        switch (this.state) {
+            case this.STATE_STARTED : {
+                this.state = this.STATE_STOPPING; 
+                if (this.doStop()) {
+                    for (let i=0; i<this.subscribers.length; i++) if (this.subscribers[i] !== undefined) unsubscribe( this.subscribers[i] );
+                    this.subscribers = [];
+                    for (let i=0; i<this.schedulers.length; i++) if (this.schedulers[i] !== undefined) clearSchedule( this.schedulers[i] );
+                    this.schedulers = [];
+                    this.state = this.STATE_STOPPED; 
+                    this.log('script stopped');
+                }
+                break;    
+            }
+            default : {
+                this.log('cant stopp script, because not startet');
+            }
+        } 
     }
     
-    // --------------------- virtual functions, overwrite it 
-    
+    //
+    // virtual functions, overwrite it 
+    //
     doInit() { return true; }
     doStart() { return true; }
     doStop() { return true; }
     
-    // --------------------- helper functions 
+    // einen on-Handler registrieren
+    subscribe( handler ) {
+        this.subscribers.push( handler );
+    }
     
+    // einen timer registrieren
+    schedule( handler ) {
+        this.schedulers.push( handler );
+    }
+    
+    //
+    // tool functions 
+    //
     logDebug(msg) { if (this.DEBUG) console.log('['+this.NAME+'] '+msg); }
     log(msg) { console.log('['+this.NAME+'] '+msg); }
     logWarn(msg) { console.warn('['+this.NAME+'] '+msg); }
@@ -176,11 +243,8 @@ class MduiBase {
       this.TR064_INSTANCE = 'tr-064.0.';
     
       // var
-      this.onChangeReachableHandler = undefined;
-      this.onChangeSortByHandler  = undefined;
-      this.onChangeSortAscendingHandler  = undefined;
-      this.sortBy = '';
-      this.sortAscending = true;
+      this.logs = [];
+      this.logs.push( {sortByOld : '', sortAscending : true});
     
       // states
       this.states.push( { id:'list',     common:{name:'HTML code for vis list', write:false, def:''} } );
@@ -197,12 +261,9 @@ class MduiBase {
     doStart() { 
       super.doStart();
       // on-Handler
-      this.onChangeReachableHandler  = on( {id:new RegExp( '^'+this.TR064_INSTANCE+'devices.*.active' ), change:'ne'}, obj => { this.onChangeReachable(obj) } );
-      this.onChangeSortByHandler = on( this.STATE_PATH+'sortBy', obj => { this.onChangeSortBy(obj) } );
-      this.onChangeSortAscendingHandler = on( this.STATE_PATH+'sortAscending', obj => { this.onChangeSortAscending(obj) } );
-      // vars
-      this.sortBy = this.getState('sortBy').val;
-      this.sortAscending = this.getState('sortAscending').val;
+      this.subscribe( on( {id:new RegExp( '^'+this.TR064_INSTANCE+'devices.*.active' ), change:'ne'}, obj => { this.onChangeReachable(obj) } ) );
+      this.subscribe( on( {id: this.STATE_PATH+'sortBy', change: "any"} , obj => { this.onChangeSortBy(obj) } ));
+      this.subscribe( on( this.STATE_PATH+'sortAscending', obj => { this.onChangeSortAscending(obj) } ) );
     
       this.updateHTML();
       return true; 
@@ -211,19 +272,21 @@ class MduiBase {
     //
     doStop() { 
       super.doStop();
-      if (this.onChangeReachableHandler!==undefined) onLogUnregister(this.onChangeReachableHandler);
-      if (this.onChangeSortByHandler!==undefined) unsubscribe(this.onChangeSortByHandler);
-      if (this.onChangeSortAscendingHandler!==undefined) unsubscribe(this.onChangeSortAscendingHandler);
-    
       return true; 
     }
-    
+    //
+    // beim gleichen sortBy Sortierreihenfolge umdrehen
+    // 
     onChangeSortBy(obj) {
-      this.sortBy = this.getState('sortBy').val;
+      if (this.logs[0].sortByOld == obj.state.val) {
+          this.setState('sortAscending', !this.logs[0].sortAscending);
+          return;
+      }      
+      this.logs[0].sortByOld = obj.state.val;
       this.updateHTML();
     }
+    
     onChangeSortAscending (obj) {
-      this.sortAscending = this.getState('sortAscending').val;
       this.updateHTML();
     }
     
@@ -241,33 +304,53 @@ class MduiBase {
           let active = getState( device+'.active' ).val;
           // IP für evtl. sort aufbereiten 000.000.000.000
           let ip = getState( device+'.lastIP' ).val.split('.');
-          ip = ip[0].padStart(3, '0') + '.' +  ip[1].padStart(3, '0') + '.' +  ip[2].padStart(3, '0') + '.' + ip[3].padStart(3, '0');
-          let mac = getState( device+'.lastMAC-address').val;
+          if (ip.length>=3)
+              ip = ip[0].padStart(3, '0') + '.' +  ip[1].padStart(3, '0') + '.' +  ip[2].padStart(3, '0') + '.' + ip[3].padStart(3, '0');
+          let mac = '';
+          if (existsState(device+'.lastMAC-address') )  
+              mac = getState( device+'.lastMAC-address').val;
           let lastActiveTS = getState( device+'.lastActive-ts' ).val;
           json.push( {'ip':ip, 'name':device.substr(device.lastIndexOf('.')+1,255), 'active':active, 'mac':mac, 'ts':formatDate(lastActiveTS, "TT.MM SS:mm:ss")} );
       } catch(err) { this.logError( 'updateHTML: '+err.message ); }
-      json.sort( this.compareNodes.bind(this) );
-      this.convertJSON2HTML(json);
-    } catch(err) { this.logError( 'onChangeReachable: '+err.message ); }  }
+    
+      let log = this.logs[0];
+      log.sortBy = '';
+      log.sortAscending = true;
+      log.statePath = this.STATE_PATH;
+    
+      if (this.existState('sortBy')) log.sortBy = this.getState('sortBy').val;
+      if (this.existState('sortAscending')) log.sortAscending = this.getState('sortAscending').val;
+      log.sortbyactive = 'mdui-sortable';
+      log.sortbyname = 'mdui-sortable';
+      log.sortbyip = 'mdui-sortable';
+      log.sortbymac = 'mdui-sortable';
+      log.sortbyts = 'mdui-sortable';
+    
+      if ( log.sortBy!='')
+          if (log.sortAscending) log['sortby'+log.sortBy] = 'mdui-sort-ascending';
+          else log['sortby'+log.sortBy] = 'mdui-sort-descending';
+      
+          json.sort(  (l,r) => {
+                let lv=l['name'],rv=r['name'];
+                if (l.hasOwnProperty(log.sortBy)) lv=l[log.sortBy];
+                if (r.hasOwnProperty(log.sortBy)) rv=r[log.sortBy];
+                return ((lv < rv) ? -1 : (lv > rv) ? 1 : 0) * (log.sortAscending?1:-1);
+          }  );
+      
+      this.convertJSON2HTML(json, log);
+    } catch(err) { this.logError( 'updateHTML: '+err.message ); }  }
+    
     
     //
-    compareNodes(l,r) {
-      let lv=l['name'],rv=r['name'];
-      if (l.hasOwnProperty(this.sortBy)) lv=l[this.sortBy]+lv;
-      if (r.hasOwnProperty(this.sortBy)) rv=r[this.sortBy]+rv;
-      return ((lv < rv) ? -1 : (lv > rv) ? 1 : 0) * (this.sortAscending?1:-1);
-    }
-    
-    //
-    convertJSON2HTML(json) {
+    convertJSON2HTML(json, log) {
     const tmpTable = {
     header : 
     `<tr>
-    <th style='text-align:left;'>Verb.?</th>
-    <th style='text-align:left;'>Name</th>
-    <th style='text-align:left;'>IP</th>
-    <th style='text-align:left;'>MAC</th>
-    <th style='text-align:left;'>geändert am</th>
+    <th class="{sortbyactive}" style="text-align:left;" onclick="vis.setValue('`+log.statePath+`sortBy','active');">Verb.?</th>
+    <th class="{sortbyname}" style="text-align:left;" onclick="vis.setValue('`+log.statePath+`sortBy','name');">Name</th>
+    <th class="{sortbyip}" style="text-align:left;" onclick="vis.setValue('`+log.statePath+`sortBy','ip');">IP</th>
+    <th class="{sortbymac}"  style="text-align:left;" onclick="vis.setValue('`+log.statePath+`sortBy','mac');">MAC</th>
+    <th class="{sortbyts}"  style="text-align:left;" onclick="vis.setValue('`+log.statePath+`sortBy','ts');">geändert am</th>
     </tr>`,
     row : 
     `<tr>
@@ -293,6 +376,7 @@ class MduiBase {
         let htmlList  = "";
         let entry, tr;
         let unreachable = 0;
+        for (let [key, value] of Object.entries(log)) htmlTable = htmlTable.replace(new RegExp('{'+key+'}','g'),value);
     
         for (var i = 0; i < json.length; i++) { 
             entry = json[i];
@@ -301,10 +385,10 @@ class MduiBase {
                 default     : entry.icon = 'check_circle_outline'; entry.icon_color='mdui-green'; break;
             }
             tr = tmpTable.row;    
-            for (let [key, value] of Object.entries(entry)) tr = tr.replace('{'+key+'}',value);
+            for (let [key, value] of Object.entries(entry)) tr = tr.replace(new RegExp('{'+key+'}','g'),value);
             htmlTable+=tr;
             tr = tmpList.row;    
-            for (let [key, value] of Object.entries(entry)) tr = tr.replace('{'+key+'}',value);
+            for (let [key, value] of Object.entries(entry)) tr = tr.replace(new RegExp('{'+key+'}','g'),value);
             htmlList+=tr;
         }
         htmlTable+="</body></table>";    
